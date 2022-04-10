74a9be018bfa1b018ff3ac7c217a0d5021bd22ba
dev0ps221 <seybaxscorpion@gmail.com>
2022-04-10 03:38:25 +0000

    refactor

--- ears.js
+++ ears.js
@@ -1,22 +1,48 @@
 class Ears{
     //Evts
     registerTrigger(tn,cb){
-        let callback = cb
-        this.triggers[tn] = {
+      let callback = cb
+      let trigger = null
+      if(this.triggers.hasOwnProperty(tn)) trigger = this.triggers[tn]
+      if(trigger){
+        const cbs = trigger.getCb()
+        trigger.getCb = ()=>{
+          return [callback].concat(cbs)
+        }
+      }else{
+        trigger = {
             name:tn,
             getCb:()=>{
-                return callback
+                return [callback]
             }
         }
+      }
+      this.triggers[tn] = trigger
     }
-    forgetTrigger(tn){
-        if(this.triggers.hasOwnProperty(tn)) delete(this.triggers[tn])
+    forgetTrigger(tn,idx){
+        if(this.triggers.hasOwnProperty(tn)){
+          let cbs = []
+          this.triggers[tn].getCb().map(
+            (t,i)=>{
+              if(i+1!=idx){
+                t = function(){}
+              }
+              cbs.push(t)
+            }
+          )
+          this.triggers[tn].getCb = ()=>{
+            return cbs
+          }
+        }
     }
     registerCallBack(tn,cb){
-        this.callbacks[tn] = cb
+        let callback = null
+        if(this.callbacks.hasOwnProperty(tn)) callback = this.callbacks[tn]
+        this.callbacks[tn] = callback ? [cb].concat(callback) : [cb]
+        return this.callbacks[tn].length
     }
-    forgetCallBack(tn){
-        if(this.callbacks.hasOwnProperty(tn)) delete(this.callbacks[tn])
+    forgetCallBack(tn,idx){
+        if(this.callbacks.hasOwnProperty(tn)) delete(this.callbacks[tn][idx])
     }
     removeAction(an){
         if(this.actions.hasOwnProperty(an)) delete(this.actions[an])
@@ -25,20 +51,20 @@ class Ears{
         this.actions[an] = af
     }
     getAction(an){
-        return this.actions[an] ? this.actions[an] : null 
+        return this.actions[an] ? this.actions[an] : null
     }
     do(an,...args){
         const action = this.getAction(an)
-        !action ? action : action(...args)
+        return !action ? action : action(...args)
     }
-    getCallBack(tn){
-        return this.callbacks.hasOwnProperty(tn)?this.callbacks[tn]:null
+    getCallBack(tn,idx){
+        return this.callbacks.hasOwnProperty(tn)?this.callbacks[tn][idx-1]:null
     }
     getTrigger(tn){
         return this.triggers.hasOwnProperty(tn)?this.triggers[tn]:null
     }
     doTrigger(tn,trigger,data){
-        this.triggered[tn]={trigger,data}
+        this.triggered[tn] = (this.triggered.hasOwnProperty(tn)) ? this.triggered[tn].concat([{trigger,data}]) : [{trigger,data}]
     }
     undoTrigger(tn){
         if(this.getTrigger(tn)) delete(this.getTrigger(tn))
@@ -47,22 +73,27 @@ class Ears{
         const actions = [
             [
                 'on',(triggername,triggercb)=>{
-                    this.registerCallBack(triggername,triggercb)
-                    this.registerTrigger(triggername,this.getCallBack(triggername))
+                    const idx = this.registerCallBack(triggername,triggercb)
+                    this.registerTrigger(triggername,this.getCallBack(triggername,idx))
+                    return idx
                 }
             ],
             [
-                'off',(triggername)=>{
-                    this.forgetCallBack(triggername)
-                    this.forgetTrigger(triggername)
-                    this.undoTrigger(triggername)
+                'off',(triggername,idx)=>{
+                    this.forgetCallBack(triggername,idx)
+                    this.forgetTrigger(triggername,idx)
+                    this.undoTrigger(triggername,idx)
                 }
             ],[
                 'trigger',(triggername,...data)=>{
                     let interv = setInterval(
                         ()=>{
                             if(this.getTrigger(triggername)){
-                                this.doTrigger(triggername,this.getTrigger(triggername).getCb(),data)
+                                this.getTrigger(triggername).getCb().map(
+                                  triggerfunc=>{
+                                    this.doTrigger(triggername,triggerfunc,data)
+                                  }
+                                )
                                 clearInterval(interv)
                             }
                         }
@@ -81,7 +112,11 @@ class Ears{
             Object.keys(this.triggers).forEach(
                 tn=>{
                     if(this.triggered.hasOwnProperty(tn)){
-                        this.triggered[tn].trigger(...this.triggered[tn].data)
+                        this.triggered[tn].map(
+                          trigger=>{
+                            trigger.trigger(...trigger.data)
+                          }
+                        )
                         delete(this.triggered[tn])
                     }
                 }
@@ -116,7 +151,7 @@ class Ear{
         else    this.when('ready',cb)
     }
     when(context,action){
-        this.on(context,action)
+        return this.on(context,action)
     }
     listenReady(){
         let readyinterval = null
@@ -130,19 +165,19 @@ class Ear{
         )
     }
     on(an,fn){
-        
-        this.evts.do("on",an,fn)
+
+        return this.evts.do("on",an,fn)
 
     }
     isReady(){
-        return this.ready 
+        return this.ready
     }
-    off(an){
-        
-        this.evts.do("off",an)
+    off(an,idx){
+
+        this.evts.do("off",an,idx)
     }
-    forget(an){
-        this.evts.do("off",an)
+    forget(an,idx){
+        this.evts.do("off",an,idx)
     }
     trigger(an,...args){
         this.evts.do("trigger",an,...args)
@@ -157,15 +192,14 @@ class Ear{
         this.ready = true
     }
     static build(...obj){
-        
+
         return Object.assign(new Ear(),...obj)
     }
-    constructor(){  
+    constructor(){
       this.evts   = new Ears()
       this.ready = false
       this.listenReady()
     }
-
 }
 if((typeof module) != 'undefined'){
     module.exports = {
